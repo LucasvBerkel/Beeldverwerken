f478c7391609bf3551d8caac1aeb5c12e8c3c824
lucasvberkel <lucasvberkel@gmail.com>
2016-04-24 15:55:14 +0200

    Klein test scriptje

---
+++ test.m
@@ -0,0 +1,17 @@
+
+% sum(sum(Gauss(1)))
+% sum(sum(Gauss(5)))
+% sum(sum(Gauss(10)))
+% mesh(Gauss(3))
+
+
+Gauss1(5)
+Gauss(5)
+
+% im = rgb2gray(im2double(imread('cameraman.jpg')));
+% 
+% h = imfilter(im, Gauss(100), 'conv', 'replicate');
+% imshow(h)
+% figure;
+% b = imfilter(im, Gauss(2), 'conv', 'replicate');
+% imshow(b);
